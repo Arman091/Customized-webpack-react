41f6f236107b988ef767081421cd25dea493e97f
Arman091 <rasoolarman7@gmail.com>
2022-10-01 18:00:17 +0530

    first commit

---
+++ Webpack/Component/First.js
@@ -0,0 +1,8 @@
+import React from "react"
+export function First() {
+    
+    return (
+        <h2 className="head">this is our first component</h2>
+        // you can can render this component in src/App.js
+    )
+}
---
+++ Webpack/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { First } from '../Component/First'
+
+function App() {
+    
+    return (
+        <div>
+       <h1>hello we are here</h1>
+       <First />
+        </div>
+    )
+}
+
+export default App;
